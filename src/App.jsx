56558048e4cb5654b0d3a767611344809365104a
sunskycloud <q5503212@gmail.com>
2024-07-11 02:10:35 +0300

    working

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Spotlight from "./components/Spotlight";
+
+function App() {
+    return (
+        <>
+            <Spotlight />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Spotlight.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useState } from "react";
+import "./Spotlight.css";
+
+const Spotlight = () => {
+    const divRef = useRef(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [opacity, setOpacity] = useState(0);
+
+    const handleMouseMove = (e) => {
+        if (!divRef.current || isFocused) return;
+
+        const div = divRef.current;
+        const rect = div.getBoundingClientRect();
+
+        setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+    };
+
+    const handleFocus = () => {
+        setIsFocused(true);
+        setOpacity(1);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+        setOpacity(0);
+    };
+
+    const handleMouseEnter = () => {
+        setOpacity(1);
+    };
+
+    const handleMouseLeave = () => {
+        setOpacity(0);
+    };
+
+    return (
+        <div
+            ref={divRef}
+            onMouseMove={handleMouseMove}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="card-spotlight-effect"
+        >
+            <div
+                className="spotlight"
+                style={{
+                    opacity,
+                    background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(255,255,255,.06), transparent 40%)`,
+                }}
+            />
+        </div>
+    );
+};
+export default Spotlight;
